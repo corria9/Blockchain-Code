ac0b653cc3c17a8bdd4e357b8be4ac0cc71ff1f5
Armando Coria <48301256+corria9@users.noreply.github.com>
2020-06-11 21:50:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,10 +10,36 @@ class Block {
 	}
 
 	createHash () {
-		return SHA256(this.index + this.date + this.data);
+		return SHA256(this.index + this.date + this.data).toString();
 	}
 }
 
-block = new Block (0, 'prueba');
-console.log(JSON.stringify(block, null, 2));
 
+//Objeto que se crea para recoger nuestro blockchain
+
+class BlockChain {
+    constructor(genesis) {
+        this.chain = [this.createFirstBlock(genesis)];
+    }
+    createFirstBlock(genesis) {
+        return new Block (0, genesis);
+    }
+    getLastBlock() {
+        return this.chain[this.chain.length -1];
+    }
+    addBlock(data) {
+        let prevBlock = this.getLastBlock();
+        let block = new Block(prevBlock.index +1, data, prevBlock.hash);
+        this.chain.push(block);
+    }
+}
+
+// block = new Block (0, 'prueba');
+// console.log(JSON.stringify(block, null, 2));
+
+let coriaCoin = new BlockChain ('info de genesis');
+
+coriaCoin.addBlock('esta es mi CryptoMoneda!');
+coriaCoin.addBlock('Tengo un Valor de 100K pesos');
+
+console.log(JSON.stringify(coriaCoin, null, 2));
